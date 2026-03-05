ddb3694f823cd9952fbab8b06f5fadbafe7b6525
fayssal90210 <21298962+BFMIX@users.noreply.github.com>
2026-03-05 02:45:10 +0100

    Security: Move Firebase secrets to .env and update .gitignore

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,15 +8,15 @@ import {
 } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
-// TODO: Remplacer ces valeurs par celles de votre projet Firebase (Google Cloud Console)
+// On utilise les variables d'environnement Expo (préfixées par EXPO_PUBLIC_)
 const firebaseConfig = {
-  apiKey: "AIzaSyCcqVG4HKsMNx1eqjep2ssyKnNVw5PrvAQ",
-  authDomain: "sos-toilettes-366f8.firebaseapp.com",
-  projectId: "sos-toilettes-366f8",
-  storageBucket: "sos-toilettes-366f8.firebasestorage.app",
-  messagingSenderId: "152185885855",
-  appId: "1:152185885855:web:3e2e2f06fbbb2dd917feaa",
-  measurementId: "G-JWDD07F81C",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const isFirebaseConfigured = firebaseConfig.apiKey !== "YOUR_API_KEY";
